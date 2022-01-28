2058bede0ee76dff57baf4d0fb4f3b053ce814e9
Cort Schaefer <cort@eschaefer.net>
2022-01-28 14:01:12 -0700

    switch restsharp for httpclient because of restsharp versioning concerns

--- src/Cortside.Health/Checks/UrlCheck.cs
+++ src/Cortside.Health/Checks/UrlCheck.cs
@@ -1,31 +1,39 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Cortside.Health.Enums;
 using Cortside.Health.Models;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
-using RestSharp;
 
 namespace Cortside.Health.Checks {
     public class UrlCheck : Check {
-
         public UrlCheck(IMemoryCache cache, ILogger<Check> logger, IAvailabilityRecorder recorder) : base(cache, logger, recorder) { }
 
         public override async Task<ServiceStatusModel> ExecuteAsync() {
-            var client = new RestClient();
-            var request = new RestRequest(check.Value, Method.GET);
-            request.Timeout = (int)TimeSpan.FromSeconds(check.Timeout).TotalMilliseconds;
-
-            var response = await client.ExecuteTaskAsync(request);
-
-            return new ServiceStatusModel() {
-                Healthy = response.IsSuccessful,
-                Status = response.IsSuccessful ? ServiceStatus.Ok : ServiceStatus.Failure,
-                StatusDetail = response.IsSuccessful ? "Successful" : response.ErrorMessage,
+            var status = new ServiceStatusModel() {
+                Healthy = false,
+                Status = ServiceStatus.Failure,
+                StatusDetail = "Failed to process response",
                 Timestamp = DateTime.UtcNow,
                 Required = check.Required
             };
+
+            var httpClient = new HttpClient {
+                Timeout = TimeSpan.FromSeconds(check.Timeout)
+            };
+
+            using (var response = await httpClient.GetAsync(check.Value, HttpCompletionOption.ResponseHeadersRead)) {
+                if (response.IsSuccessStatusCode) {
+                    status.Healthy = true;
+                    status.Status = ServiceStatus.Ok;
+                    status.StatusDetail = "Successful";
+                } else {
+                    status.StatusDetail = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return status;
         }
     }
 }
-
