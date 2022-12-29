bb1ebd5a6bc6ebb3f2273a0ed4ca36ecd1e07cc7
Cort Schaefer <cort@eschaefer.net>
2022-12-28 22:38:40 -0700

    add servicecollection extension method to register health services

---
+++ src/Cortside.Health/HealthConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Cortside.Health.Models;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Cortside.Health {
+    public class HealthConfiguration {
+        public TelemetryConfiguration TelemetryConfiguration { get; set; }
+        public HealthCheckServiceConfiguration ServiceConfiguration { get; set; }
+        public BuildModel BuildModel { get; set; }
+        public Dictionary<string, Type> CustomChecks { get; set; }
+    }
+}
---
+++ src/Cortside.Health/ServiceCollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Cortside.Health.Checks;
+using Cortside.Health.Recorders;
+using Microsoft.ApplicationInsights;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Cortside.Health {
+    public static class ServiceCollectionExtensions {
+        public static IServiceCollection AddHealth(this IServiceCollection services, HealthConfiguration configuration) {
+            if (!string.IsNullOrEmpty(configuration.TelemetryConfiguration?.ConnectionString)) {
+                TelemetryClient telemetryClient = new TelemetryClient(configuration.TelemetryConfiguration);
+                services.AddSingleton(telemetryClient);
+                services.AddTransient<IAvailabilityRecorder, ApplicationInsightsRecorder>();
+            } else {
+                services.AddTransient<IAvailabilityRecorder, NullRecorder>();
+            }
+
+            // configuration
+            services.AddSingleton(configuration.ServiceConfiguration);
+            services.AddSingleton(configuration.BuildModel);
+
+            // checks
+            services.AddTransient<UrlCheck>();
+            services.AddTransient<DbContextCheck>();
+            foreach (var check in configuration.CustomChecks) {
+                services.AddTransient(check.Value);
+            }
+
+            // check factory and hosted service
+            services.AddSingleton(sp => {
+                var cache = sp.GetService<IMemoryCache>();
+                var logger = sp.GetService<ILogger<Check>>();
+                var recorder = sp.GetService<IAvailabilityRecorder>();
+                var settings = sp.GetService<IConfiguration>();
+
+                var factory = new CheckFactory(cache, logger, recorder, sp, settings);
+                foreach (var check in configuration.CustomChecks) {
+                    factory.RegisterCheck(check.Key, check.Value);
+                }
+                return factory as ICheckFactory;
+            });
+            services.AddHostedService<HealthCheckHostedService>();
+
+            return services;
+        }
+    }
+}
